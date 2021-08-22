3927ab497e9099c20f9789eda4c6bf6e67f8dc5c
Faay94 <86892203+Faay94@users.noreply.github.com>
2021-08-21 20:12:26 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+token = "1976060288:AAGG6mWq2q078MVKKBSja-zMzjDx-Tz2dlE" # Enter your Token
+api_id = 6351477 # Enter your api_id
+api_hash = "b0a5a5a31e3689319b1e397fc6479ee2" # Enter your api_hash
+sudo = []
+id_bot = "@TikTokTS_Bot"
+channel = "@ancientb"
